3c0bc0c36dd9d319dd27a516781666ef7b40f5c8
unknown <emmanuelmbewe180@gmail.com>
2025-12-21 14:37:40 +0200

    Added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,8 @@ import Layout from "./components/Layout";
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-const MyApp = ({ Component, pageProps }) => {
+function MyApp({ Component, pageProps }) {
+   console.log("GA ID from env:", process.env.NEXT_PUBLIC_GA_ID);
   return (
     <>
       <Head>
@@ -14,28 +15,33 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Google Analytics Script */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
+      {/* Google Analytics */}
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
 
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      )}
 
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
   );
-};
+}
 
 export default MyApp;
